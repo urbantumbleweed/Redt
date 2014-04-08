d976e7559c94b57f079511cb4fd4180963c35240
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-08 19:31:25 -0400

    rails g model generations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id         :integer          not null, primary key
+#  score      :integer
+#  user_id    :integer
+#  link_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :link
