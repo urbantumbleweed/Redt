5bd8ef190e8d97a69c64f6df3f0ecdb7ee0e307e
urbantumbleweeed <saunders.stephen@gmail.com>
2014-04-08 22:05:22 -0400

    wrote the basic crud methods for users

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: links
+#
+#  id         :integer          not null, primary key
+#  link_url   :string(255)
+#  title      :string(255)
+#  tally      :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Link < ActiveRecord::Base
 end
