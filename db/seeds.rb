08486c30198de52486cc0be1b3adac075b50eeb7
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-10 02:54:59 -0400

    add user and link seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# # # This file should contain all the record creation needed to seed the database with its default values.
+# # # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# # #
+# # # Examples:
+# # #
+# # #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+# # #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# # user4=User.create(id:4,email:"stephencolbert@gmail.com",username:"stephencolbert",password:"password1",password_confirmation:"password1")
+# # user5=User.create(id:5,email:"doge@gmail.com",username:"doge",password:"password1",password_confirmation:"password1")
+# user6=User.create(id:6,email:"haxxer@gmail.com",username:"haxxer",password:"password1",password_confirmation:"password1")
+# # user7=User.create(id:7,email:"dmiller@gmail.com", username: "dmiller",password:"password1",password_confirmation:"password1")
+# # user8=User.create(id:8,email:"fashionista@gmail.com",username:"fashionista",password:"password1",password_confirmation:"password1")
+# # user9=User.create(id:9,email:"baslansdorp@gmail.com",username:"baslansdorp",password:"password1",password_confirmation:"password1")
+
+# # link1=Link.create(title:"I Am America (And So Can You!)",link_url:"http://www.amazon.com/Am-America-So-Can-You/dp/0446582182",user_id:4,tally:64)
+# # link2=Link.create(title:"such space",link_url:"http://media.giphy.com/media/YRjZCWEIqMuEU/giphy.gif",user_id:5,tally:9001)
+# # link3=Link.create(title:"Bueller?",link_url:"https://www.youtube.com/watch?v=f4zyjLyBp64",user_id:5,tally:33)
+# # link4=Link.create(title:"Heartbleed Alert!",link_url:"http://mashable.com/2014/04/09/heartbleed-bug-websites-affected/?utm_cid=mash-com-g+-pete-link",user_id:6,tally:111)
+# # link5=Link.create(title:"Jammer Security Issue",link_url:"http://jammer.herokuapp.com/",user_id:7,tally:23)
+# # link6=Link.create(title:"Sweatpant jeans",link_url:"http://www.rag-bone.com/Digital_Rock_withHoles/pd/np/3000/p/6891.html?utm_source=pjn&utm_medium=affiliate&utm_campaign=20648&cvosrc=affiliate.pepperjam.20648",user_id:8,tally:44)
+# # link7=Link.create(title:"It's Friday!",link_url:"http://a.fod4.com/misc/dancedance.gif",user_id:7,tally:51)
+# # link8=Link.create(title:"Get yr ticket to mars",link_url:"https://www.mars-one.com/",user_id:9,tally:100)
+# # link9=Link.create(title:"cat in onesie",link_url:"http://i.imgur.com/yTgpyFx.jpg",user_id:5,tally:428)
+
+# # user4.links << link1
+# # user5.links << link2 << link3 << link9
+# # user6.links << link4
+# # user7.links << link5 << link7
+# # user8.links << link6
+# # user9.links << link8
