3614169a497c065240ebce37c1031df1b908b628
urbantumbleweeed <saunders.stephen@gmail.com>
2014-04-09 21:21:47 -0400

    wrote the methods for the voting functionality

--- app/models/link.rb
+++ app/models/link.rb
@@ -22,34 +22,75 @@ class Link < ActiveRecord::Base
   validates(:tally,numericality: {only_integer: true})
 
 
-  def check_upvote(user)
-    vote = Vote.where("link_id = ? AND user_id = ?", self.id, user.id).first
-    if vote
-      vote.update(:score=>vote.score +=1) if vote.score < 1
-      return false
-    elsif !user.votes.include?(self.id)
-    Vote.create(:user_id => user.id,
-      :link_id=>self.id, :score => 1)
-    return true
+  def create_vote(user, score)
+    Vote.create(:user_id => user.id, :link_id => self.id, :score => score)
+  end
+
+  def find_vote(user)
+    Vote.where("link_id = ? AND user_id = ?", self.id, user.id).first
+  end
+
+  def upvote
+    return true if (score_change > 0 && vote && vote.score == 1)
+  end
+
+  def downvote
+    return true if (score_change < 0 && vote && vote.score == -1)
+  end
+
+  def score_link(user, score_change)
+    vote = self.find_vote(user)
+
+    if vote == nil
+        self.create_vote(user, score_change)
+        self.tally += score_change
+    elsif self.upvote || self.downvote
+      vote.score += score_change
+      self.tally += score_change
+    elsif vote.score == score_change
+      return "Already voted"
     end
   end
 
 
 
-  def upvote(user)
-    if check_upvote(user)
-    self.update(:tally => (self.tally+=1))
-    else
-    return "Already voted"
-    end
-  end
+  # def upvote(user)
+  #   if check_upvote(user)
+  #   self.update(:tally => (self.tally+=1))
+  #   else
+  #   return "Already voted"
+  #   end
+  # end
 
-  def downvote(user)
-    self.update(:tally => (self.tally-=1))
-    if user.vote.includes? (self.id)
-      Vote.update(:score=> score-1)
-    end
-  end
+  # def downvote(user)
+  #   self.update(:tally => (self.tally-=1))
+  #   if user.vote.includes? (self.id)
+  #     Vote.update(:score=> score-1)
+  #   end
+  # end
+
+  # def check_upvote(user)
+  #   vote = Vote.where("link_id = ? AND user_id = ?", self.id, user.id).first
+  #   if vote
+  #     vote.update(:score=>vote.score +=1) if vote.score < 1
+  #     return false
+  #   elsif !user.votes.include?(self.id)
+  #   Vote.create(:user_id => user.id,
+  #     :link_id=>self.id, :score => 1)
+  #   return true
+  #   end
+  # end
+
+  # def check_downvote(user)
+  #   vote = Vote.where("link_id = ? AND user_id = ?", self.id, user.id).first
+  #   if vote
+  #     vote.update(:score => vote.score -=1) if vote.score > -1
+  #     return false
+  #   elsif !user.votes.include?(self.id)
+  #     Vote.create(:user_id => user.id, :link_id => self.id, :score => -1)
+  #     return true
+  #   end   
+  # end
 
 
 end
